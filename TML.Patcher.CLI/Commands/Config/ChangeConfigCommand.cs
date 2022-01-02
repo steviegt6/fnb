e0e15f7c0c84eb378363d4ecea885773f169460c
Steviegt6 <xxlennygamerxx@gmail.com>
2022-01-02 01:26:54 -0800

    Added simple config options.

---
+++ TML.Patcher.CLI/Commands/Config/ChangeConfigCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using TML.Patcher.CLI.Configuration;
+
+namespace TML.Patcher.CLI.Commands.Config
+{
+    [Command]
+    public class ChangeConfigCommand : ICommand
+    {
+        [CommandOption("storage-path", Description = "Sets the game storage path (where worlds, players, etc. are),")]
+        public string? NewStoragePath { get; init; } = null;
+
+        [CommandOption("steam-path", Description = "Sets the Steam/GoG game path.")]
+        public string? NewSteamPath { get; init; } = null;
+
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            if (NewStoragePath is not null)
+            {
+                Program.Runtime!.ProgramConfig.StoragePath = NewStoragePath;
+                await console.Output.WriteLineAsync("Set game storage to: " + NewSteamPath);
+            }
+
+            if (NewSteamPath is not null)
+            {
+                Program.Runtime!.ProgramConfig.SteamPath = NewSteamPath;
+                await console.Output.WriteLineAsync("Set steam path to: " + NewSteamPath);
+            }
+            
+            ProgramConfig.SerializeConfig(Program.Runtime!.ProgramConfig, Program.Runtime.PlatformStorage);
+        }
+    }
+}
