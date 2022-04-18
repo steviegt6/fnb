0f400488d32b190968a81c78bdf30c9d1e1245d6
Steviegt6 <xxlennygamerxx@gmail.com>
2022-04-17 21:56:11 -0700

    Fix up credits.

--- TML.Patcher.Client/Commands/Informative/CreditsCommand.cs
+++ TML.Patcher.Client/Commands/Informative/CreditsCommand.cs
@@ -20,13 +20,12 @@ namespace TML.Patcher.Client.Commands.Informative
 
 Special thanks:[white]
  - [yellow]Trivaxy[/], for providing me with the original code for unpacking .tmod files. While it has since been rewritten, it is what prompted me to start this project in the first place.
- - [yellow]Archanyhm[/], for assisting me with Linux and Mac support for the older implementation of IL Spy decompilation..
+ - [yellow]Archanyhm[/] & [yellow]Metacinnabar[/], for assisting me with Linux and Mac support for the older implementation of ILSpy decompilation.
  - [yellow]Chik3r[/], for tons of help with multithreading, unmanaged code, decompilation, maintenance, and more.[/]
 
  * Release Notes *
 
-[silver]
-Current - v1.0.1
+[silver]Current - v1.0.1
  - Updated path logic to support April 2022's re-standardization of paths.
  - Added a big warning telling people to use ILSpy for decompiling.
  - Some simple error handling for end-user mistakes.
