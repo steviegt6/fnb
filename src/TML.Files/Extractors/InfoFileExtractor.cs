c63361894bcca3932172bf2353c375dc73f015d2
Tomat <xxlennygamerxx@gmail.com>
2022-08-14 16:45:37 -0700

    Rewrite build.txt/Info reading.

--- src/TML.Files/Extractors/InfoFileExtractor.cs
+++ src/TML.Files/Extractors/InfoFileExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using TML.Files.Abstractions;
 
@@ -7,6 +8,96 @@ namespace TML.Files.Extractors
 {
     public class InfoFileExtractor : IFileExtractor
     {
+        protected readonly record struct InfoKey(string Key, InfoKey.ReadInfoValue Reader)
+        {
+            public delegate void ReadInfoValue(BinaryReader reader, ref string tag, out string? value);
+
+            public static InfoKey List(string key) {
+                return new InfoKey(
+                    key,
+                    (BinaryReader reader, ref string _, out string? value) => { value = string.Join(", ", ReadList(reader)); }
+                );
+            }
+
+            public static InfoKey True(string key) {
+                return new InfoKey(
+                    key,
+                    (BinaryReader reader, ref string _, out string? value) => { value = "true"; }
+                );
+            }
+
+            public static InfoKey False(string key) {
+                return new InfoKey(
+                    key,
+                    (BinaryReader reader, ref string tag, out string? value) =>
+                    {
+                        tag = tag[1..];
+                        value = "false";
+                    }
+                );
+            }
+
+            public static InfoKey Side(string key) {
+                return new InfoKey(
+                    key,
+                    (BinaryReader reader, ref string tag, out string? value) =>
+                    {
+                        value = reader.ReadByte() switch
+                        {
+                            0 => "Both",
+                            1 => "Client",
+                            2 => "Server",
+                            3 => "NoSync",
+                            _ => null,
+                        };
+                    }
+                );
+            }
+
+            public static InfoKey Skip(string key) {
+                return new InfoKey(key, (BinaryReader reader, ref string _, out string? value) => { value = null; });
+            }
+
+            public static InfoKey String(string key) {
+                return new InfoKey(
+                    key,
+                    (BinaryReader reader, ref string tag, out string? value) => { value = reader.ReadString(); }
+                );
+            }
+        }
+
+        protected static InfoKey[] Keys =
+        {
+            InfoKey.List("dllReferences"),
+            InfoKey.List("modReferences"),
+            InfoKey.List("weakReferences"),
+            InfoKey.List("sortAfter"),
+            InfoKey.List("sortBefore"),
+
+            InfoKey.True("noCompile"),
+            InfoKey.True("includeSource"),
+            InfoKey.True("includePDB"),
+            InfoKey.True("beta"),
+
+            InfoKey.False("!hideCode"),
+            InfoKey.False("!hideResources"),
+            InfoKey.False("!playableOnPreview"),
+
+            InfoKey.Side("side"),
+
+            // This is identical to description.txt, no point in actually reading it.
+            InfoKey.Skip("description"),
+
+            // TODO: eacPath and buildVersion aren't properties set by the user. Should we still include these? Probably not...
+            // InfoKey.String("eacPath"),
+            // InfoKey.String("buildVersion"),
+            InfoKey.String("displayName"),
+            InfoKey.String("author"),
+            InfoKey.String("version"),
+            InfoKey.String("homepage"),
+            InfoKey.String("languageVersion"),
+        };
+
         public bool ShouldExtract(IModFileEntry fileEntry) {
             // build.txt is turned into the "Info" file in the root directory.
             return fileEntry.Name == "Info";
@@ -18,47 +109,12 @@ namespace TML.Files.Extractors
             using BinaryReader reader = new(stream);
 
             for (string tag = reader.ReadString(); tag.Length > 0; tag = reader.ReadString()) {
-                string? value = null;
-                switch (tag) {
-                    case "dlLReferences" or "modReferences" or "weakReferences" or "sortAfter" or "sortBefore":
-                        value = string.Join(", ", ReadList(reader));
-                        break;
-
-                    case "noCompile" or "includeSource" or "includePDB" or "beta":
-                        value = "true";
-                        break;
-
-                    case "!hideCode" or "!hideResources" or "!playableOnPreview":
-                        value = "false";
-                        tag = tag[1..];
-                        break;
-
-                    case "side":
-                        value = reader.ReadByte() switch
-                        {
-                            0 => "Both",
-                            1 => "Client",
-                            2 => "Server",
-                            3 => "NoSync",
-                            _ => null,
-                        };
-                        break;
-
-                    // Junk data, already saved to description.txt anyway.
-                    case "description":
-                        break;
-
-                    // TODO: eacPath and buildVersion aren't properties set by the user. Should we still include these?
-                    // case "eacPath":
-                    // case "buildVersion":
-                    case "displayName":
-                    case "author":
-                    case "version":
-                    case "homepage":
-                    case "languageVersion":
-                        value = reader.ReadString();
-                        break;
-                }
+                InfoKey key = Keys.FirstOrDefault(x => x.Key == tag);
+                string? value;
+                
+                // TODO: Throw an exception or raise some sort of message if no reader for the tag is found?
+                if (key == default) value = null;
+                else key.Reader(reader, ref tag, out value);
 
                 if (value is not null) sb.AppendLine($"{tag} = {value}");
             }
