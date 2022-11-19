14fd5e97e845ce404b483a24d0976a1985e721a2
Tomat <xxlennygamerxx@gmail.com>
2022-11-18 22:02:05 -0800

    Prepare for CLI app.

---
+++ src/TML.Patcher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+
+namespace TML.Patcher;
+
+public static class Program
+{
+    public static async Task<int> Main(string[] args) {
+        return await new CliApplicationBuilder()
+                    .AddCommandsFromThisAssembly()
+                    .Build()
+                    .RunAsync(args);
+    }
+}
