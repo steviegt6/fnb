eb611884ed2d9eece6dee2c94a73954fa5ff5e99
Tomat <xxlennygamerxx@gmail.com>
2022-11-22 19:58:23 -0800

    Remove ./nocompile/.

--- nocompile/Patcher/Patching/PatchStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Patcher.Patching
-{
-    /// <summary>
-    ///     Represents a patch status, including whether the patch has been applied and if it was successful.
-    /// </summary>
-    /// <param name="Applied">Whether the patch has been applied.</param>
-    /// <param name="Success">Whether the patch application was successful.</param>
-    public readonly record struct PatchStatus(bool Applied, bool Success);
-}
--- nocompile/TML.Patcher.Client/Platform/Mac/MacStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TML.Patcher.Client.Platform.Mac
-{
-    /// <summary>
-    ///     The default <see cref="Storage"/> object for MacOS computers.
-    /// </summary>
-    public class MacStorage : UnixStorage
-    {
-    }
-}
