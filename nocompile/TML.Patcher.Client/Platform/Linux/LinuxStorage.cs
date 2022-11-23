186a144bb1709208e645beef8e7cb7b5e9f87015
Tomat <xxlennygamerxx@gmail.com>
2022-11-22 20:22:00 -0800

    Slightly Reorganization and Other Stuff (#32)

--- nocompile/TML.Patcher.Client/Platform/Linux/LinuxStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TML.Patcher.Client.Platform.Linux
-{
-    /// <summary>
-    ///     The default <see cref="Storage"/> object for Linux computers.
-    /// </summary>
-    public class LinuxStorage : UnixStorage
-    {
-    }
-}
