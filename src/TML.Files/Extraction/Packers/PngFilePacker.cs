423fef7f9f9de7a53f1014d5e2e1a8bfada21b67
LoLXD8783 <56768047+LoLXD8783@users.noreply.github.com>
2023-02-26 04:16:02 -0600

    Use Capacity

--- src/TML.Files/Extraction/Packers/PngFilePacker.cs
+++ src/TML.Files/Extraction/Packers/PngFilePacker.cs
@@ -31,8 +31,8 @@ public class PngFilePacker : BaseFilePacker
         writer.Write(image.Height);
         int totalPixels = image.Width * image.Height;
 
-        if (to.Length < totalPixels) {
-            to.SetLength(totalPixels);
+        if (to.Capacity < totalPixels) {
+            to.Capacity = totalPixels;
         }
 
         if (to.TryGetBuffer(out ArraySegment<byte> buffer) && buffer.Count >= totalPixels && image.DangerousTryGetSinglePixelMemory(out var memory)) {
