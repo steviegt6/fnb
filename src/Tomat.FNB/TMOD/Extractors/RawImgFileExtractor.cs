cd43ab666d294f9978197f546c53d208ed26924a
Tomat <xxlennygamerxx@gmail.com>
2023-11-10 00:55:02 -0800

    Maybe faster?

--- src/Tomat.FNB/TMOD/Extractors/RawImgFileExtractor.cs
+++ src/Tomat.FNB/TMOD/Extractors/RawImgFileExtractor.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace Tomat.FNB.TMOD.Extractors;
 
-public sealed class RawImgFileExtractor : FileExtractor {
+public sealed unsafe class RawImgFileExtractor : FileExtractor {
     public override bool ShouldExtract(TmodFileEntry entry) {
         return Path.GetExtension(entry.Path) == ".rawimg";
     }
 
     public override TmodFileData Extract(TmodFileEntry entry, byte[] data) {
-        ReadOnlySpan<byte> span = data;
-        var width = MemoryMarshal.Read<int>(span.Slice(4, 4));
-        var height = MemoryMarshal.Read<int>(span.Slice(8, 4));
-        var rgbaValues = data.AsMemory(12);
+        fixed (byte* ptr = data) {
+            var width = Unsafe.ReadUnaligned<int>(ptr + 4);
+            var height = Unsafe.ReadUnaligned<int>(ptr + 8);
+            var rgbaValues = ptr + 12;
 
-        using var image = Image.WrapMemory<Rgba32>(Configuration.Default, rgbaValues, width, height);
+            using var image = Image.WrapMemory<Rgba32>(rgbaValues, data.Length - 12, width, height);
 
-        using var ms = new MemoryStream();
-        image.SaveAsPng(ms);
-        return new TmodFileData(Path.ChangeExtension(entry.Path, ".png"), ms.ToArray());
+            using var ms = new MemoryStream();
+            image.SaveAsPng(ms);
+            return new TmodFileData(Path.ChangeExtension(entry.Path, ".png"), ms.ToArray());
+        }
     }
 }
