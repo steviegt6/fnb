f1b53810d95cde52ac7da1cd78f4ee11a17631f2
Tomat <xxlennygamerxx@gmail.com>
2023-11-09 23:42:14 -0800

    Also remove `tmod list` for now due to the multiple resolution strategies and lack of usefulness.

--- src/Tomat.FNB/Commands/TMOD/TmodListCommand.cs
+++ src/Tomat.FNB/Commands/TMOD/TmodListCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿/*
+ using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
@@ -13,3 +14,6 @@ public class TmodListCommand : ICommand {
         throw new System.NotImplementedException();
     }
 }
+*/
+
+
