133308e9c01dd86678b112d3f8e7675989158c7c
Tomat <xxlennygamerxx@gmail.com>
2023-11-09 23:11:12 -0800

    Implement local listing.

--- src/Tomat.FNB/Commands/CommandUtil.cs
+++ src/Tomat.FNB/Commands/CommandUtil.cs
@@ -7,7 +7,62 @@ using Microsoft.Win32;
 namespace Tomat.FNB.Commands;
 
 internal static class CommandUtil {
+    private static readonly string[] mod_loader_dir_candidates = { "ModLoader", "tModLoader", "tModLoader-1.4.3", "tModLoader-dev", "tModLoader-preview" };
+
     public const int TMODLOADER_APPID = 1281930;
+
+    public static bool TryGetLocalTmodArchives([NotNullWhen(returnValue: true)] out Dictionary<string, Dictionary<string, string>>? localMods) {
+        if (!TryGetTerrariaStoragePath(out var storageDir)) {
+            localMods = null;
+            return false;
+        }
+
+        localMods = new Dictionary<string, Dictionary<string, string>>();
+
+        foreach (var candidate in mod_loader_dir_candidates) {
+            var dir = Path.Combine(storageDir, candidate);
+            if (!Directory.Exists(dir))
+                continue;
+
+            var modsDir = Path.Combine(dir, "Mods");
+            if (!Directory.Exists(modsDir))
+                continue;
+
+            var modsMap = new Dictionary<string, string>();
+
+            foreach (var mod in Directory.EnumerateFiles(modsDir, "*.tmod", SearchOption.TopDirectoryOnly)) {
+                var modName = Path.GetFileName(mod);
+                modsMap.Add(modName, mod);
+            }
+
+            localMods.Add(candidate, modsMap);
+        }
+
+        return true;
+    }
+
+    public static bool TryGetTerrariaStoragePath([NotNullWhen(returnValue: true)] out string? storageDir) {
+        storageDir = null;
+
+        if (OperatingSystem.IsWindows()) {
+            storageDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "My Games");
+        }
+        else if (OperatingSystem.IsLinux()) {
+            if (Environment.GetEnvironmentVariable("XDG_DATA_HOME") is not { } xdgDataHome)
+                storageDir = Environment.GetEnvironmentVariable("HOME") is not { } home ? "." : Path.Combine(home, ".local", "share");
+            else
+                storageDir = xdgDataHome;
+        }
+        else if (OperatingSystem.IsMacOS()) {
+            storageDir = Environment.GetEnvironmentVariable("HOME") is not { } home ? "." : Path.Combine(home, "Library", "Application Support");
+        }
+
+        if (storageDir is null)
+            return false;
+
+        storageDir = Path.Combine(storageDir, "Terraria");
+        return true;
+    }
 
     public static Dictionary<string, TmodWorkshopRecord> ResolveTmodWorkshopEntries(string workshopDir) {
         var map = new Dictionary<string, TmodWorkshopRecord>();
--- src/Tomat.FNB/Commands/TMOD/TmodListLocalsCommand.cs
+++ src/Tomat.FNB/Commands/TMOD/TmodListLocalsCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
@@ -7,7 +9,21 @@ namespace Tomat.FNB.Commands.TMOD;
 
 [Command("tmod list-locals", Description = "Lists known, locally-installed .tmod files")]
 public class TmodListLocalsCommand : ICommand {
-    public ValueTask ExecuteAsync(IConsole console) {
-        throw new System.NotImplementedException();
+    public async ValueTask ExecuteAsync(IConsole console) {
+        if (!CommandUtil.TryGetLocalTmodArchives(out var localMods)) {
+            await console.Output.WriteLineAsync("No local mods found or local installation directory could not be resolved.");
+            return;
+        }
+
+        var totalArchives = localMods.Sum(x => x.Value.Count);
+        await console.Output.WriteLineAsync($"Found {totalArchives} {(totalArchives == 1 ? "mod" : "mods")} installed locally:");
+
+        foreach (var (branchName, archives) in localMods) {
+            await console.Output.WriteLineAsync($"    {branchName} ({archives.Count} {(archives.Count == 1 ? "mod" : "mods")}):");
+
+            foreach (var archive in archives) {
+                await console.Output.WriteLineAsync($"        {Path.GetFileName(archive.Value)}");
+            }
+        }
     }
 }
