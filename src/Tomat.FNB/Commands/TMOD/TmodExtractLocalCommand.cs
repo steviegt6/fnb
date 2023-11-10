16f543f169c6409fb317cdd62f7e452f0730e322
Tomat <xxlennygamerxx@gmail.com>
2023-11-09 23:26:05 -0800

    Get the searching set up for local extraction.

--- src/Tomat.FNB/Commands/TMOD/TmodExtractLocalCommand.cs
+++ src/Tomat.FNB/Commands/TMOD/TmodExtractLocalCommand.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
@@ -9,7 +12,62 @@ namespace Tomat.FNB.Commands.TMOD;
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
 [Command("tmod extract-local", Description = "Extracts a mod installed locally in your tModLoader installation")]
 public class TmodExtractLocalCommand : ICommand {
-    public ValueTask ExecuteAsync(IConsole console) {
-        throw new System.NotImplementedException();
+    [UsedImplicitly(ImplicitUseKindFlags.Access | ImplicitUseKindFlags.Assign)]
+    [CommandParameter(0, Name = "name", Description = "The .tmod file name, with or without the .tmod extension", IsRequired = true)]
+    public string TmodName { get; set; } = null!;
+
+    [UsedImplicitly(ImplicitUseKindFlags.Access | ImplicitUseKindFlags.Assign)]
+    [CommandParameter(1, Name = "branch", Description = "The branch the mod is installed to, if multiple files with the same name exist (tModLoader, ModLoader, tModLoader-1.4.3, etc.)", IsRequired = false)]
+    public string? TmodBranch { get; set; }
+
+    [UsedImplicitly(ImplicitUseKindFlags.Access | ImplicitUseKindFlags.Assign)]
+    [CommandOption("out-dir", 'o', Description = "The directory to extract the mod to. If not specified, it will be extracted to ./<mod name>", IsRequired = false)]
+    public string? OutputDirectory { get; set; }
+
+    public async ValueTask ExecuteAsync(IConsole console) {
+        if (!CommandUtil.TryGetLocalTmodArchives(out var localMods)) {
+            await console.Output.WriteLineAsync("No local mods found or local installation directory could not be resolved.");
+            return;
+        }
+
+        var archives = new Dictionary<string, string>();
+
+        foreach (var (branchName, branchArchives) in localMods) {
+            foreach (var (archiveName, archivePath) in branchArchives) {
+                if (archiveName == TmodName || archiveName == $"{TmodName}.tmod")
+                    archives.Add(branchName, archivePath);
+            }
+        }
+
+        switch (archives.Count) {
+            case 0:
+                await console.Output.WriteLineAsync($"No local mods found with the name \"{TmodName}\".");
+                return;
+
+            case 1:
+                await ExtractArchive(console, archives.Values.Single(), OutputDirectory);
+                return;
+
+            default:
+                if (TmodBranch is null) {
+                    await console.Output.WriteLineAsync($"Multiple local mods found with the name \"{TmodName}\". Please specify a branch to extract from.");
+                    return;
+                }
+
+                if (!archives.TryGetValue(TmodBranch, out var archivePath)) {
+                    await console.Output.WriteLineAsync($"No local mods found with the name \"{TmodName}\" and branch \"{TmodBranch}\".");
+                    return;
+                }
+
+                await ExtractArchive(console, archivePath, OutputDirectory);
+                return;
+        }
+    }
+
+    private static async ValueTask ExtractArchive(IConsole console, string archivePath, string? destinationPath) {
+        destinationPath ??= Path.GetFileNameWithoutExtension(archivePath);
+        await console.Output.WriteLineAsync($"Extracting \"{archivePath}\" to \"{destinationPath}\"...");
+
+        // TODO: extract
     }
 }
