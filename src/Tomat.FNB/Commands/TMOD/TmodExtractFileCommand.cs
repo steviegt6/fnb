3cf42164762696e8d38a6b9494129aa6535f5a99
Tomat <xxlennygamerxx@gmail.com>
2023-11-10 18:22:38 -0800

    Additional formatting.

--- src/Tomat.FNB/Commands/TMOD/TmodExtractFileCommand.cs
+++ src/Tomat.FNB/Commands/TMOD/TmodExtractFileCommand.cs
@@ -1,4 +1,5 @@
-﻿/*using System.Threading.Tasks;
+﻿/*
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
--- src/Tomat.FNB/Commands/TMOD/TmodListCommand.cs
+++ src/Tomat.FNB/Commands/TMOD/TmodListCommand.cs
@@ -1,5 +1,5 @@
 ﻿/*
- using System.Threading.Tasks;
+using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
