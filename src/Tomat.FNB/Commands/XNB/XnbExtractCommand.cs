03fe12b5103a539130f8df4776ce5379a5999bec
ppebb <ppeb@ppeb.me>
2024-03-17 22:53:25 -0500

    Comment unimplemented commands and set version string so fnb can be packaged

--- src/Tomat.FNB/Commands/XNB/XnbExtractCommand.cs
+++ src/Tomat.FNB/Commands/XNB/XnbExtractCommand.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Threading.Tasks;
-using CliFx;
-using CliFx.Attributes;
-using CliFx.Infrastructure;
-using JetBrains.Annotations;
-
-namespace Tomat.FNB.Commands.XNB;
-
-[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
-[Command("xnb extract", Description = "Extracts an XNB file or directory or XNB files into their original formats")]
-public sealed class XnbExtractCommand : ICommand {
-    public ValueTask ExecuteAsync(IConsole console) {
-        throw new NotImplementedException();
-    }
-}
+﻿// using System;
+// using System.Threading.Tasks;
+// using CliFx;
+// using CliFx.Attributes;
+// using CliFx.Infrastructure;
+// using JetBrains.Annotations;
+//
+// namespace Tomat.FNB.Commands.XNB;
+//
+// [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+// [Command("xnb extract", Description = "Extracts an XNB file or directory or XNB files into their original formats")]
+// public sealed class XnbExtractCommand : ICommand {
+//     public ValueTask ExecuteAsync(IConsole console) {
+//         throw new NotImplementedException();
+//     }
+// }
