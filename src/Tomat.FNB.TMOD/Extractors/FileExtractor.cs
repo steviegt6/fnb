11be361c757507576318940b38be5744475160ab
tomat <xxlennygamerxx@gmail.com>
2024-04-25 18:39:24 -0700

    Remove existing FNB.TMOD files, copy fpng to output dir.

--- src/Tomat.FNB.TMOD/Extractors/FileExtractor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tomat.FNB.TMOD.Extractors;
-
-public abstract class FileExtractor {
-    public abstract bool ShouldExtract(TmodFile.Entry entry);
-
-    public abstract TmodFile.Data Extract(TmodFile.Entry entry, byte[] data);
-}
--- src/Tomat.FNB.TMOD/Packers/FilePacker.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tomat.FNB.TMOD.Packers;
-
-public abstract class FilePacker {
-    public abstract bool ShouldPack(TmodFileData data);
-
-    public abstract TmodFileData Pack(TmodFileData data);
-}
