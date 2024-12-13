84e1da42b5588f9d737a1ec136a223e67c15b281
Stephen T <xxlennygamerxx@gmail.com>
2024-12-13 03:27:58 -0600

    Drastically simplify handling and use `png` crate.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,72 +1,32 @@
-use core::slice;
 use std::ptr;
 
-use image::{codecs::png::PngEncoder, ImageEncoder, RgbaImage};
 use libc::size_t;
 
 #[no_mangle]
-pub extern "C" fn decode_png(data: *const u8, length: size_t) -> *mut RgbaImage
-{
-    if data.is_null() || length == 0 {
-        return ptr::null_mut();
-    }
+pub extern "C" fn encode_png(width: u32, height: u32, data: *const u8, length: *mut size_t) -> *mut u8 {
+    // encode a raw image of width x height into a png using the png crate
 
-    let slice = unsafe { slice::from_raw_parts(data, length) };
-
-    match image::load_from_memory_with_format(slice, image::ImageFormat::Png) {
-        Ok(image) => {
-            let rgba_image = image.to_rgba8();
-            Box::into_raw(Box::new(rgba_image))
-        }
-        Err(_) => ptr::null_mut(),
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn encode_png(image: *const RgbaImage, length: *mut size_t) -> *mut u8 {
-    if image.is_null() {
-        return ptr::null_mut();
-    }
-
-    let rgba_image = unsafe { &*image };
-
+    let data = unsafe { std::slice::from_raw_parts(data, (width * height * 4) as usize) };
     let mut buf = Vec::new();
-    let encoder = PngEncoder::new(&mut buf);
-    match encoder.write_image(&rgba_image, rgba_image.width(), rgba_image.height(), image::ExtendedColorType::Rgba8) {
-        Ok(_) => {
-            unsafe {
-                *length = buf.len() as size_t;
-            }
-            let data = buf.as_mut_ptr();
-            std::mem::forget(buf);
-            data
-        }
-        Err(_) => ptr::null_mut(),
-    }
-}
+    let mut encoder = png::Encoder::new(&mut buf, width, height);
+    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(data).unwrap();
+    writer.finish().unwrap();
 
-#[no_mangle]
-pub extern "C" fn create_image_from_raw_data(width: size_t, height: size_t, data: *const u8) -> *mut RgbaImage {
-    if data.is_null() {
+    unsafe { *length = buf.len() as size_t }
+
+    let ptr = unsafe { libc::malloc(*length) as *mut u8 };
+    if ptr.is_null() {
         return ptr::null_mut();
     }
 
-    let slice = unsafe { slice::from_raw_parts(data, (width * height * 4) as usize) };
-
-    let image = RgbaImage::from_raw(width as u32, height as u32, slice.to_vec());
-    match image {
-        Some(image) => Box::into_raw(Box::new(image)),
-        None => ptr::null_mut(),
+    unsafe {
+        ptr::copy_nonoverlapping(buf.as_ptr(), ptr, *length);
     }
-}
 
-#[no_mangle]
-pub extern "C" fn free_decoded_image(image: *mut RgbaImage) {
-    if !image.is_null() {
-        unsafe {
-            drop(Box::from_raw(image));
-        }
-    }
+    ptr
 }
 
 #[no_mangle]
--- src/Tomat.FNB.TMOD/Converters/Extractors/ImageFnbExtractor.cs
+++ src/Tomat.FNB.TMOD/Converters/Extractors/ImageFnbExtractor.cs
@@ -18,28 +18,40 @@ internal sealed unsafe partial class ImageFnbExtractor : IFileConverter
             var height = *(int*)(pData + 8);
             var pImage = pData + 12;
 
-            var image     = create_image_from_raw_data(width, height, pImage);
-            var pPng      = encode_png(image, out var length);
+            // var image     = create_image_from_raw_data(width, height, pImage);
+            // var pPng      = encode_png(image, out var length);
+            // var imageData = new byte[length];
+            // Marshal.Copy((nint)pPng, imageData, 0, (int)length);
+            // free_encoded_png(pPng);
+            // free_decoded_image(image);
+            // return (Path.ChangeExtension(path, ".png"), imageData);
+
+            var pPng      = encode_png(width, height, pImage, out var length);
             var imageData = new byte[length];
             Marshal.Copy((nint)pPng, imageData, 0, (int)length);
             free_encoded_png(pPng);
-            free_decoded_image(image);
             return (Path.ChangeExtension(path, ".png"), imageData);
         }
     }
 
 #region image_fnb
-    [LibraryImport("image_fnb", EntryPoint = "decode_png")]
-    private static partial nint decode_png(byte* data, nint length);
+    // [LibraryImport("image_fnb", EntryPoint = "decode_png")]
+    // private static partial nint decode_png(byte* data, nint length);
+
+    // [LibraryImport("image_fnb", EntryPoint = "encode_png")]
+    // private static partial byte* encode_png(nint image, out nint length);
+
+    // [LibraryImport("image_fnb", EntryPoint = "create_image_from_raw_data")]
+    // private static partial nint create_image_from_raw_data(nint width, nint height, byte* data);
+
+    // [LibraryImport("image_fnb", EntryPoint = "free_decoded_image")]
+    // private static partial void free_decoded_image(nint image);
+
+    // [LibraryImport("image_fnb", EntryPoint = "free_encoded_png")]
+    // private static partial void free_encoded_png(byte* data);
 
     [LibraryImport("image_fnb", EntryPoint = "encode_png")]
-    private static partial byte* encode_png(nint image, out nint length);
-
-    [LibraryImport("image_fnb", EntryPoint = "create_image_from_raw_data")]
-    private static partial nint create_image_from_raw_data(nint width, nint height, byte* data);
-
-    [LibraryImport("image_fnb", EntryPoint = "free_decoded_image")]
-    private static partial void free_decoded_image(nint image);
+    private static partial byte* encode_png(nint width, nint height, byte* data, out nint length);
 
     [LibraryImport("image_fnb", EntryPoint = "free_encoded_png")]
     private static partial void free_encoded_png(byte* data);
