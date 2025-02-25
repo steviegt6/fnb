b324d1a997d063e6e12d4691fc6546bfaad8b665
Stephen T <xxlennygamerxx@gmail.com>
2025-02-24 22:04:49 -0600

    Set up basic CLI app.

---
+++ src/Tomat.FNB.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+using CliFx;
+
+namespace Tomat.FNB.CLI;
+
+internal static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        return await new CliApplicationBuilder()
+                    .SetExecutableName("fnb")
+                    .SetTitle("fnb")
+                    .SetDescription(
+                         "tmod & fnb file packer and unpacker"
+                       + "\nCopyright (C) 2025  Tomat et al."
+                     )
+                    .Build()
+                    .RunAsync(args);
+    }
+}
