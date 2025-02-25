d96a8c5463c2ac0e7e2aa701831452477739ac6a
Stephen T <xxlennygamerxx@gmail.com>
2025-02-24 21:58:29 -0600

    Clear project files.

--- src/Tomat.FNB.Common.Deflate/Usings.cs
+++
@@ -1,4 +0,0 @@
-// We can easily configure these to be int, long, or nint as needed.
-
-global using SSize = int;
-global using USize = uint;
--- src/Tomat.FNB.TMOD/Converters/IFileConverter.cs
+++
@@ -1,8 +0,0 @@
-namespace Tomat.FNB.TMOD.Converters;
-
-public interface IFileConverter
-{
-    bool ShouldConvert(string path, byte[] data);
-
-    (string path, byte[] data) Convert(string path, byte[] data);
-}
